fffe06f910b1845f2ad36323b12d0ddf04140a84
dajung <yd1065@naver.com>
2021-06-21 22:58:00 +0900

    비디오 업로드 페이지 스크립트 입력 가능하도록 수정 및 드롭다운 클릭 가능

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 //ant design 에서 css form을 가져옴
 import {Typography, Button, Form, message, Input, Icon} from "antd";
 import Dropzone from 'react-dropzone';
@@ -7,9 +7,44 @@ import {getIn} from "formik";
 const {TextArea} = Input;
 const {Title} = Typography;
 
+const PrivateOptions = [
+    {value : 0, label : "Private"},
+    {value : 1, label : "Public"}
+]
+
+const CategoryOptions = [
+    {value : 0, label : "Film & Animation"},
+    {value : 1, label : "Auto & Vehicles"},
+    {value : 2, label : "Music"},
+    {value : 3, label : "Pets & Animals"}
+]
+
 
 //functional component 생성
 function VideoUploadPage() {
+
+    const [VideoTitle, setVideoTitle] = useState("")
+    const [Description, setDescription] = useState("")
+    const [Private, setPrivate] = useState(0) //private : 0 , public : 1
+    const [Category, setCategory] = useState("Film & Animation")
+
+    const onTitleChange = (e) => {
+        //e : 뭔가 이벤트가 발생했을때(한번 입력할때)의 변경된 state
+        setVideoTitle(e.currentTarget.value)
+    }
+
+    const onDescriptionChange = (e) => {
+        setDescription(e.currentTarget.value)
+    }
+
+    const onPrivateChange = (e) => {
+        setPrivate(e.currentTarget.value)
+    }
+
+    const onCategoryChange = (e) => {
+        setCategory(e.currentTarget.value)
+    }
+
     return (
         <div style={{ maxWidth:'700px', margin:'2rem auto' }}>
             <div style={{ textAlign:'center', marginBottom:'2rem' }}>
@@ -39,26 +74,32 @@ function VideoUploadPage() {
                 <br />
                 <label>Title</label>
                 <Input
-                    onChange
-                    value
+                    onChange={onTitleChange}
+                    value={VideoTitle}
                 />
                 <br />
                 <br />
                 <label>Description</label>
                 <TextArea
-                    onChange
-                    value
+                    onChange={onDescriptionChange}
+                    value={Description}
                 />
                 <br />
                 <br />
 
-                <select onChange>
-                    <option key value></option>
+                {/*private or public select box*/}
+                <select onChange={onPrivateChange}>
+                    {/*map을 이용해서 option 바를 더 가독성 있게 사용*/}
+                    {PrivateOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
                 </select>
                 <br />
                 <br />
-                <select onChange>
-                    <option key value></option>
+                <select onChange={onCategoryChange}>
+                    {CategoryOptions.map((item, index) => (
+                        <option key={index} value={item.value}>{item.label}</option>
+                    ))}
                 </select>
                 <br />
                 <br />
