c431cb5e5ed6a546e2fb2e50816a13df5919158f
dajung <yd1065@naver.com>
2021-07-04 13:54:27 +0900

    sidevideo 모두 가져와서 정보노출되도록 수정(map 이용)

--- client/src/components/views/VideoDetailPage/Sections/SideVideo.js
+++ client/src/components/views/VideoDetailPage/Sections/SideVideo.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import Axios from "axios";
 
 function SideVideo() {
-    return(
-        <div style={{display:'flex', marginBottom:"1rem", padding:"0 2rem"}}>
-            <div style={{width:"40%", marginBottom:"1rem"}}>
-                 <a href>
-                     <img style={{width:"100%"}} src alt />
-                 </a>
+
+    const [sideVideos, setSideVideos] = useState([])
+
+    useEffect(() => {
+        Axios.get('/api/video/getVideos')
+            .then(response => {
+                if(response.data.success) {
+                    console.log(response.data)
+                    setSideVideos(response.data.videos)
+                } else {
+                    alert("비디오 가져오기에 실패했습니다.")
+                }
+            })
+    }, [])
+
+    const renderSideVideo = sideVideos.map((video, index) => {
+        var minutes = Math.floor(video.duration / 60)
+        var seconds = Math.floor(video.duration - minutes * 60);
+
+        return <div key={index} style={{display:'flex', marginBottom:"1rem", padding:"0 2rem"}}>
+            <div style={{width:"40%", marginBottom:"1rem", marginRight:"1rem"}}>
+                <a href>
+                    <img style={{width:"100%", height:"100%"}} src={`http://localhost:5000/${video.thumbnail}`} alt="thumbnail" />
+                </a>
             </div>
             <div style={{width:'50%'}}>
-                <a href>
-                    <span style={{fontsize:"1rem", color:'black'}}>videoTitle</span><br />
-                    <span>이름</span><br />
-                    <span>조회수</span><br />
-                    <span>상영시간</span>
+                <a href style={{color:'gray'}}>
+                    <span style={{fontsize:"1rem", color:'black'}}>{video.title}</span><br />
+                    <span>{video.writer.name}</span><br />
+                    <span>{video.views} views</span><br />
+                    <span>{minutes} : {seconds}</span>
                 </a>
 
             </div>
         </div>
+    })
+
+    return(
+        <React.Fragment>
+            <div style={{marginTop:"3rem"}}/>
+            {renderSideVideo}
+        </React.Fragment>
 
     )
 }
